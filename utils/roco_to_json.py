2816dbf0d49a257b9602cfe143ca91c1873ec6f7
Muhammad Gema Akbar <mgemaakbar@gmail.com>
2024-12-03 19:53:21 +0100

    add id key to json result

--- utils/roco_to_json.py
+++ utils/roco_to_json.py
@@ -9,7 +9,7 @@ def rocov_csv_to_json():
         reader = csv.DictReader(csvfile)
         for row in reader:
             randomQuestion = random.choice(js)['conversations'][0]['value'] # take random question
-            jeson = {"image": row['ID'], "conversations": [{"from": "human", "value": randomQuestion}, {"from": "gpt", "value": row['Caption']}]}
+            jeson = {"id": "?", "image": row['ID'], "conversations": [{"from": "human", "value": randomQuestion}, {"from": "gpt", "value": row['Caption']}]}
             result.append(jeson)
     print (json.dumps(result, indent=1, separators=(',', ': ')))
                 
