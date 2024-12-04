a9cfa71c94144157793946b79eae96cd3375a38a
Momo Takamatsu <takamamo@strauss.ims.uni-stuttgart.de>
2024-12-04 19:56:58 +0100

    revised data_conversiona and converted datasets

--- data_process/data_conversion.py
+++ data_process/data_conversion.py
@@ -3,33 +3,53 @@ import os
 import argparse
 import json
 
+
+# create directory for dataset
+new_ds_dir = input("Enter directory name for new dataset: ")
+FOLDER = 'data/'+str(new_ds_dir)
+os.makedirs(FOLDER, exist_ok=True)
+print(f'Created: {os.path.abspath(new_ds_dir)}')
+
+
+# Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_cache_dir', type=str, default='data/data_cache', help='Directory to cache datasets')
-parser.add_argument('--folder_path', type=str, default='data/VQA-RAD', help='Directory to save images and jsonl')
+parser.add_argument('--folder_path', type=str, default=FOLDER, help='Directory to save images and jsonl')
+parser.add_argument('--chosen_ds', type=str, help='Choose number: 1-VQA, 2-SLAKE, 3-PathVQA.')
 parser.add_argument('--split', type=str, default='test', help='Split of the dataset')
 parser.add_argument('--image', action='store_true', help='Save images')
 parser.add_argument('--qa', action='store_true', help='Save question-answer pairs')
 args = parser.parse_args()
 
-data_cache_dir = '/mount/studenten-temp1/users/linchi/2024WS-FM/datasets/data_cache'
-ds_vqa_rad = load_dataset("flaviagiammarino/vqa-rad", cache_dir=data_cache_dir)
-folder_path = '/mount/studenten-temp1/users/linchi/2024WS-FM/datasets/VQA-RAD'
 
+# Paths to download datsets
+if args.chosen_ds == '1': # VQA-RAD
+    ds =load_dataset("flaviagiammarino/vqa-rad", cache_dir=args.data_cache_dir)
+elif args.chosen_ds == '2': # SLAKE
+    ds =load_dataset("mdwiratathya/SLAKE-vqa-english", cache_dir=args.data_cache_dir)
+elif args.chosen_ds == '3': # PathVQA
+    ds =load_dataset("flaviagiammarino/path-vqa", cache_dir=args.data_cache_dir)
+    
 
-# Function to save images
+# Save images
 def save_images(dataset, folder_path, split='train'):
+    # Create directory of split 
     image_folder = os.path.join(folder_path, split)
     os.makedirs(image_folder, exist_ok=True)
+    # Get the image file and save it
     for idx, row in enumerate(dataset):
-        # Get the image file and save it
         image_path = os.path.join(image_folder, f"{idx}.png")
         image = row['image']
-        image.save(image_path)
+        # Convert CMYK to RGB if necessary
+        if image.mode == "CMYK":
+            image = image.convert("RGB")
+        # Save image
+        image.save(image_path, format="PNG")
+        
 
-# Function to classify question and answer types
+# Classify question and answer types
 def classify_question_answer(row):
     # Infer question_type based on the prompt (extract question from text)
-
     question = row['question']
     question_type = question.split()[0].upper()
     
@@ -45,7 +65,9 @@ def classify_question_answer(row):
         answer_type = "OPEN"
     
     return question, question_type, answer, answer_type
-# Function to save question-answer pairs to JSONL
+
+
+# Save question-answer pairs to JSONL
 def save_qa_to_jsonl(dataset, jsonl_path):
     with open(jsonl_path, 'w') as f:
         f.write("[")
@@ -66,17 +88,20 @@ def save_qa_to_jsonl(dataset, jsonl_path):
                 f.write(json.dumps(data) + "]")
         
 
-
+# Main work
 def main(args):
+    # Save images for specific split
     if args.image:
-        save_images(ds_vqa_rad[args.split], folder_path=folder_path, split=args.split)
+        save_images(dataset=ds[args.split], folder_path=args.folder_path, split=args.split)
 
+    # Save formatted jsonl files
     jsonl_file = "question_answer_gt.jsonl"
     jsonl_path = os.path.join(args.folder_path, "_".join([args.split, jsonl_file]))
     if args.qa:
-        save_qa_to_jsonl(ds_vqa_rad[args.split], jsonl_path)
-
+        save_qa_to_jsonl(ds[args.split], jsonl_path)
+        
     print(f"Images and question-answer pairs saved to {args.folder_path}/{args.split}")
+
 
 if __name__ == '__main__':
     main(args)
